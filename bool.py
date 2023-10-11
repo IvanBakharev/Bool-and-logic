962e75842e63fc260f0d8a9fbfdf1770bf0fa3f6
Ivan <ragna.blood82@gmail.com>
2023-10-11 20:44:48 +0300

    First

--- bool.py
+++ bool.py
@@ -1,8 +1,22 @@
-cash = int(input())
-cost = int(input())
-cassa = int(input())
 
-if (cash >= cost) and (cash - cost ) <= cassa:
-    print("Cola my")
-else:
-    print("only water")
+a = int(input("Введите число, и нажмите Enter "))
+
+if ((a%2) != 0) and ((a/2)>0):
+
+ print("положительное нечетное число")
+
+if (a%2 == 0) and ((a/2)>0):
+
+ print("положительное четное число")
+
+elif (a%2 == 0) and ((a/2)<0):
+
+ print("отрицательное четное число")
+
+elif (a%2 != 0) and ((a/2)<0):
+
+ print("отрицательное нечетное число")
+
+elif (a/2 == 0):
+
+ print("нулевое число")
